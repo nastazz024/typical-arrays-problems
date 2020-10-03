ccbd555ad835ad0e8d2d0c900c3b119b6f7e991d
Zaitsava_A <AnastasiyaZaitsava@iba.by>
2020-10-03 18:57:35 +0300

    feat: testing

--- src/index.js
+++ src/index.js
@@ -1,12 +1,49 @@
+const max = (numbers) => {
+  let memory = [];
+  if (numbers && numbers.length !== 0) {
+      for (let i = 0; i < numbers.length; i++) {
+          if (memory.length === 0) {
+              memory = numbers[i]
+          }
+          if (numbers[i] > memory) {
+              memory = numbers[i]
+          }
+      }
+      return memory
+  } else {
+      return 0
+  }
 
-exports.min = function min (array) {
-  return 0;
 }
 
-exports.max = function max (array) {
-  return 0;
+const min = (numbers) => {
+  let memory = [];
+  if (numbers && numbers.length !== 0) {
+      for (let i = 0; i < numbers.length; i++) {
+          if (memory.length === 0) {
+              memory = numbers[i]
+          }
+          if (numbers[i] < memory) {
+              memory = numbers[i]
+          }
+      }
+      return memory
+  } else {
+      return 0
+  }
 }
 
-exports.avg = function avg (array) {
-  return 0;
+const avg = (numbers) => {
+  if (numbers && numbers.length !== 0) {
+      let sum = 0
+      let myAvg = 0
+      numbers.map((item) => {
+          sum = sum + item
+      })
+      myAvg = sum / numbers.length
+
+      return myAvg
+  } else {
+      return 0
+  }
 }
